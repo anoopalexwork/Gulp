b30b41b9bf63fc0c5a73060eb909f5cfbf2611e8
Anoop Alex <anoopalexwork@gmail.com>
2021-03-10 07:54:55 +0530

    got select html list from data

--- foodlist.php
+++ foodlist.php
@@ -3,14 +3,60 @@ echo "Starting...";
 $apikey= "bDAddPeLmhGpRo1ZEvUe3C8YfNDRXk1WRsAjcqgA";
 $dom = new DOMDocument();
 if (!$dom) {echo "domdoc failed"; return;}
-else { echo "domdco pass";}
 $qfood = "eggplant";
-//$list = file_get_contents('https://api.nal.usda.gov/fdc/v1/foods/list?api_key=bDAddPeLmhGpRo1ZEvUe3C8YfNDRXk1WRsAjcqgA&query='.$qfood);
-$food = file_get_contents('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=bDAddPeLmhGpRo1ZEvUe3C8YfNDRXk1WRsAjcqgA&format=abridged&query='.$qfood);
-//if ($myhtm == null) echo "not null";
-//if ($myhtm == "") echo "not empty";
-//echo $food;
+$qlist= file_get_contents('https://api.nal.usda.gov/fdc/v1/foods/search?api_key='.$apikey.'&format=abridged&query='.$qfood);
+if (!$qlist) {echo "Search error..."; return;}
 
+$regex = "/\"fdcId\"...|\"description\".../";
+
+//echo "qlist is ".$qlist;
+$chunks = getIDList($qlist,$regex);
+if (!$chunks) {echo "getIDList failed"; return false;}
+//print_r($chunks);
+//walkList($chunks);
+cute($chunks);
+
+function getIDList ($data,$pattern){
+  $esc = preg_quote($data);
+  $list = preg_split($pattern,$esc,-1, PREG_SPLIT_NO_EMPTY);
+
+  if (!$list) { echo('preg_split failed'); return false; }
+
+  $iList = [];
+  for ($i=1;$i<count($list);$i+=2){
+    $id = $list[$i];
+    $desc = $list[$i+1];
+    $id = substr($id,0,strpos($id,","));
+    $desc = substr($desc,0,strpos($desc,"\","));
+
+
+    array_push($iList,array($id,$desc));
+
+  }
+  echo "Count is ".count($iList)."\n";
+  if (!is_array($iList)) {echo "iList failed"; return false;}
+  return $iList;
+}
+
+function cute($list){
+  $html = "<select id=\"qid\" name=\"qid\">";
+  for ($i=0;$i<count($list);$i++){
+    $html.="<option value=\"".$list[$i][0]."\">".$list[$i][1]."</option>";
+  }
+  $html.="</select>";
+  echo $html;
+}
+
+function walkList($list){
+
+  for ($i=0;$i<count($list);$i++){
+    echo $list[$i][0]." ".$list[$i][1]."<BR>";
+  }
+
+}
+
+
+/*
 $scan = preg_quote($food);
 //$pattern = "/\"fdcId\"..|\"description\"..|\"dataType\"../"; //FDC ID NumberFormatter
 $foodIDs = "/\"fdcId\"../";
@@ -35,10 +81,24 @@ for ($i=1;$i<count($reglist);$i+=2){
   $y= strpos($theid,",");
   $z = strpos($thedesc,"\",");
 
-  $theid = substr($theid,0,$y)." ";
-  $thedesc = substr($thedesc,0,$z)."<BR>";
-  echo $theid.$thedesc;
+  $theid = substr($theid,0,$y);
+  $thedesc = substr($thedesc,0,$z);
+  //echo $theid.$thedesc;
+
+  array_push($itemlist,array($theid,$thedesc));
+
 }
+
+foreach ($itemlist as $x) {
+  echo $x[0].$x[1]."<BR>";
+}
+
+/*for ($i=0;$i<count($itemlist);$i++)
+{
+  echo $itemlist[$i][0];
+  echo $itemlist[$i][1];
+  echo "<BR>";
+}*/
   //$item = array($reglist[i],$reglist[i+1]);
   /*echo $reglist[i]."<BR>".$reglist[i+1]."<BR><BR>";
   array_push($ilist,$item);
